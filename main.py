65bf3bec6ea8542b127cbfe941e6d4bdac154ae3
Jasper Pluijmers <jasper.pluijmers@NVC3819.nedap.local>
2019-07-03 14:16:13 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import time
+from rpi_ws281x import PixelStrip, Color
+
+# LED strip configuration:
+LED_COUNT = 30        # Number of LED pixels.
+LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+
+def solid(strip, color):
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+    strip.show()
+
+
+# Main program logic follows:
+if __name__ == '__main__':
+    # Create NeoPixel object with appropriate configuration.
+    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+
+    print('Press Ctrl-C to quit.')
+
+    while True:
+        solid(strip, Color(255, 0, 0))
+        time.sleep(1)
+        solid(strip, Color(0, 255, 0))
